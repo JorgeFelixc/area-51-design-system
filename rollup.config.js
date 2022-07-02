07edad3778c729650941553375bbd54229b4d59d
JorgeFelixC <jorgefelix97@gmail.com>
2022-07-02 14:39:49 -0600

    added pulled

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "@rollup/plugin-babel";
 import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: "components/index.ts",
+  input: "src/index.ts",
   output: [
     {
       file: "dist/esm/index.js",
