156c2f058c80731eb1c7803e3f11d7fd6da01fd2
kevinaclu <kevinaclu@gmail.com>
2022-07-02 20:39:41 -0600

    Change tailwind config

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <button class="bg-emerald-700 text-white p-2 px-4 rounded active:bg-emerald-900">{props.children}</button>
+    <button class="area51-bg-emerald-700 area51-text-white area51-p-2 area51-px-4 area51-rounded active:area51-bg-emerald-900">{props.children}</button>
   )
 }
 
