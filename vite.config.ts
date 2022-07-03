d6353199057f73a8b76d771d37131f33aa30d7a9
JorgeFelixC <jorgefelix97@gmail.com>
2022-07-02 20:15:33 -0600

    update vite config with dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), dts({ insertTypesEntry: true })],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
     lib: {
       entry: "src/index.tsx",
       name: "area-51",
-      fileName: (format) => `area-51.${format}.tsx`,
+      formats: ["es", "umd"],
+      fileName: (format) => `area-51.${format}.js`,
     },
     rollupOptions: {
       external: ["solid-js"],
+      output: {
+        globals: {
+          "solid-js": "solid-js",
+        },
+      },
     },
   },
 });
