c9996f36a197b0aa76fc51c49e1b74cedbcabe0a
JorgeFelixC <jorgefelix97@gmail.com>
2022-07-02 20:00:12 -0600

    removed everything about rollup and restore the vite.js and configure rollup for vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
+    lib: {
+      entry: "src/index.ts",
+      name: "area-51",
+      fileName: (format) => `area-51.${format}.tsx`,
+    },
+    rollupOptions: {
+      external: ["solid-js"],
+    },
   },
 });
