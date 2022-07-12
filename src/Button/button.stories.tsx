76bd54d98b9266ccae4e6a24dd34a0b181c89ad7
JorgeFelixC <jorgefelix97@gmail.com>
2022-07-12 10:03:58 -0600

    added new story in storybook

--- src/Button/button.stories.tsx
+++ src/Button/button.stories.tsx
@@ -5,7 +5,7 @@ export default {
   title: "Button",
 } as Meta;
 
-const Template = (args: A51ButtonProps) => (
+const PrimaryButtonTemplate = (args: A51ButtonProps) => (
   <div class="area51-space-y-4">
     <Button {...args} />
     <div class="area51-space-x-4">
@@ -16,6 +16,27 @@ const Template = (args: A51ButtonProps) => (
   </div>
 );
 
-export const PrimaryButton = Template.bind({});
+export const PrimaryButton = PrimaryButtonTemplate.bind({});
 
 PrimaryButton.args = { children: "primary", size: "md", variant: "primary" };
+
+const OutlineButtonTemplate = (args: A51ButtonProps) => (
+  <div class="area51-space-y-4">
+    <Button {...args} />
+    <div class="area51-space-x-4">
+      <Button variant="outline" size="lg">
+        Large Primary
+      </Button>
+      <Button variant="outline" size="md">
+        Medium Primary
+      </Button>
+      <Button variant="outline" size="xs">
+        Small Primary
+      </Button>
+    </div>
+  </div>
+);
+
+export const OutlineButton = OutlineButtonTemplate.bind({});
+
+OutlineButton.args = { children: "primary", size: "md", variant: "outline" };
