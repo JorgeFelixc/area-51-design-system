07ab86b18b56098bda0bd54690a12e1f9f039593
JorgeFelixC <jorgefelix97@gmail.com>
2022-07-12 09:56:55 -0600

    setup the button general props

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
-import autoprefixer from "autoprefixer";
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
 import tailwindConfig from "./tailwind.config";
+ */
+const autoprefixer = require("autoprefixer");
+const tailwind = require("tailwindcss");
+const tailwindConfig = require("./tailwind.config");
+
+//  export default {
+//  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//  plugins: [tailwind(tailwindConfig)],
+//};
+ 
+//eslint-disable-next-line no-undef
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   plugins: [tailwind(tailwindConfig), autoprefixer],
--- src/Button/button.stories.tsx
+++ src/Button/button.stories.tsx
@@ -1,11 +1,21 @@
 import { Meta } from "@storybook/html";
-import Button from ".";
+import Button, { A51ButtonProps } from ".";
 
 export default {
   title: "Button",
 } as Meta;
 
-const Template = (args: any) => <Button {...args} />;
+const Template = (args: A51ButtonProps) => (
+  <div class="area51-space-y-4">
+    <Button {...args} />
+    <div class="area51-space-x-4">
+      <Button size="lg">Large Primary</Button>
+      <Button size="md">Medium Primary</Button>
+      <Button size="xs">Small Primary</Button>
+    </div>
+  </div>
+);
 
 export const PrimaryButton = Template.bind({});
-PrimaryButton.args = { children: "primary" };
+
+PrimaryButton.args = { children: "primary", size: "md", variant: "primary" };
