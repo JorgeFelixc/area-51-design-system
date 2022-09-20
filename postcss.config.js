829fce1363015c5fb3548ea00a0b8bc5c4aa67e3
Jorge Felix Cazarez <jorgefelix97@gmail.com>
2022-09-19 20:13:31 -0600

    Setup the button general props (#7)
    * setup the button general props
    
    * added new story in storybook
    
    * moves in button and postconfig delete unecessary

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
-import autoprefixer from "autoprefixer";
-import tailwind from "tailwindcss";
-import tailwindConfig from "./tailwind.config";
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+/* Changed to require import, dont support the import mordern */
+const autoprefixer = require("autoprefixer");
+const tailwind = require("tailwindcss");
+const tailwindConfig = require("./tailwind.config");
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   plugins: [tailwind(tailwindConfig), autoprefixer],
--- src/Button/button.stories.tsx
+++ src/Button/button.stories.tsx
@@ -1,11 +1,42 @@
 import { Meta } from "@storybook/html";
-import Button from ".";
+import Button, { A51ButtonProps } from ".";
 
 export default {
   title: "Button",
 } as Meta;
 
-const Template = (args: any) => <Button {...args} />;
+const PrimaryButtonTemplate = (args: A51ButtonProps) => (
+  <div class="area51-space-y-4">
+    <Button {...args} />
+    <div class="area51-space-x-4">
+      <Button size="lg">Large Primary</Button>
+      <Button size="md">Medium Primary</Button>
+      <Button size="xs">Small Primary</Button>
+    </div>
+  </div>
+);
 
-export const PrimaryButton = Template.bind({});
-PrimaryButton.args = { children: "primary" };
+export const PrimaryButton = PrimaryButtonTemplate.bind({});
+
+PrimaryButton.args = { children: "primary", size: "md", variant: "primary" };
+
+const OutlineButtonTemplate = (args: A51ButtonProps) => (
+  <div class="area51-space-y-4">
+    <Button {...args} />
+    <div class="area51-space-x-4">
+      <Button variant="outline" size="lg">
+        Large Primary
+      </Button>
+      <Button variant="outline" size="md">
+        Medium Primary
+      </Button>
+      <Button variant="outline" size="xs">
+        Small Primary
+      </Button>
+    </div>
+  </div>
+);
+
+export const OutlineButton = OutlineButtonTemplate.bind({});
+
+OutlineButton.args = { children: "primary", size: "md", variant: "outline" };
